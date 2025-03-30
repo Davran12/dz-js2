50fd31e892bc558f49efa180142f7e575ab78095
Davran <davranlapa@gmail.com>
2025-03-30 20:32:15 +0600

    dz30

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-let a = [1, {test2: {true: false}, test: 21}, {meta: ["metalabs", 113]}]
+//! Задача #1
+let sentence =
+  "The mysterious old man whispered secrets to the curious child under the moonlight."
+let words = sentence.split(" ")
 
-let arr = [
-  "color",
-  [
-    1222,
-    {
-      meta: ["labs", 117673],
-      meta: ["labs", 110, {finally: "check", arr2: ["hello world", a]}],
-    },
-  ],
-]
+console.log(words)
+let sentence1 = words.slice(0, 9).join(" ") + "."
+let sentence2 = words.slice(0, 7).join(" ") + " under the moonlight."
+let sentence3 = words.slice(8, 11).join(" ") + " listened to the secrets."
+let sentence4 =
+  words.slice(1, 4).join(" ") + " whispered to the child under the moonlight."
 
-console.log(arr[1][1].meta[2].arr2[1][2].meta[0])
+// console.log(sentence1)
+// console.log(sentence2)
+// console.log(sentence3)
+// console.log(sentence4)
+
