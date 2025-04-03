cd09f6576b1c4aa44e71da75ce5d4430309445a9
Davran <davranlapa@gmail.com>
2025-04-03 23:49:47 +0600

    dz32

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-//! Задача #1
-// let sentence =
-//   "The mysterious old man whispered secrets to the curious child under the moonlight."
-// let words = sentence.split(" ")
+const students = [
+  {name: "Иван", age: 21, scores: [90, 85, 88]},
+  {name: "Мария", age: 23, scores: [78, 82, 80]},
+  {name: "Пётр", age: 20, scores: [92, 88, 95]},
+]
 
-// console.log(words)
-// let sentence1 = words.slice(0, 9).join(" ") + "."
-// let sentence2 = words.slice(0, 7).join(" ") + " under the moonlight."
-// let sentence3 = words.slice(8, 11).join(" ") + " listened to the secrets."
-// let sentence4 =
-//   words.slice(1, 4).join(" ") + " whispered to the child under the moonlight."
+for (let i = 0; i < students.length; i++) {
+  const student = students[i]
+  const scores = student.scores
 
-// console.log(sentence1)
-// console.log(sentence2)
-// console.log(sentence3)
-// console.log(sentence4)
-// ! Задача #2
-let world = ["The", "dog", "chased", "the", "across", "the  "]
-let world1 = prompt("Введите слово cat")
-let world2 = prompt("Введите слово yard")
-world.splice(4, 1, world1)
-world.push(world2)
-let world4 = world.join(" ")
-console.log(world4)
+  let total = 0
+  for (let j = 0; j < scores.length; j++) {
+    total += scores[j]
+  }
+
+  const dav1 = total / scores.length
+  console.log(dav1)
+
+  if (dav1 > 85) {
+      console.log(`${student.name}: ${dav1}`)
+  }
+}
