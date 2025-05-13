3a51b5c14b0670b456b78660db18b6b78cb40481
Davran <davranlapa@gmail.com>
2025-05-13 23:01:23 +0600

    dz47

--- main.js
+++ main.js
@@ -1,32 +1,14 @@
-const purchases = [
-  {name: "Книга", price: 500, quantity: 2},
-  {name: "Ручка", price: 30, quantity: 5},
-  {name: "Наушники", price: 1500, quantity: 1},
-]
+class Student {
+  constructor(name, grades) {
+    this.name = name
+    this.grades = grades
+  }
 
-let total = 0
+  getAverageGrade() {
+    const sum = this.grades.reduce((acc, grade) => acc + grade, 0)
+    return sum / this.grades.length
+  }
+}
 
-purchases.forEach((item) => {
-  const itemTotal = item.price * item.quantity
-  console.log(`${item.name}: ${item.quantity} × ${item.price} = ${itemTotal}`)
-  total += itemTotal
-})
-
-console.log(`Итоговая сумма: ${total}`)
-
-const numbers = [10, 15, 7, 20, 33, 2]
-
-const result = numbers.reduce(
-  (acc, num) => {
-    if (num % 2 === 0) {
-      acc.evenSum += num
-    } else {
-      acc.oddSum += num
-    }
-    return acc
-  },
-  {evenSum: 0, oddSum: 0}
-)
-
-console.log(`Сумма чётных: ${result.evenSum}`)
-console.log(`Сумма нечётных: ${result.oddSum}`)
+const student = new Student("Bob", [5, 4, 3, 5])
+console.log("Задача 8:", student.getAverageGrade())
