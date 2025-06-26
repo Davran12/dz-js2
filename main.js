0d7631bf54aad87682cb4f3620cdede0da267154
Davran <davranlapa@gmail.com>
2025-06-26 09:36:49 +0600

    dz64

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
-const regex = /^\+\d{1,3}[- ]?\d{6,12}$/
+// function isDateInRange(date, start, end) {
+//   const d = date.getTime()
+//   const s = start.getTime()
+//   const e = end ? end.getTime() : s
 
-console.log(regex.test("+1 1234567890"))
-console.log(regex.test("+380-987654321"))
-console.log(regex.test("+44 12345678"))
-console.log(regex.test("+1234 567890"))
-console.log(regex.test("+12_34567890"))
-//--------------------------------------------------
-const text =
-  "Today is #sunny! I love #Summer2024 and #coding. #123 is cool, but ##notvalid"
+//   return d >= s && d <= e
+// }
 
-console.log(text.match(/(?<=\s|^)(#[a-zA-Z0-9]+)\b/g))
+// const date = new Date("2024-03-15")
+// const start = new Date("2024-03-10")
+// const end = new Date("2024-03-20")
+
+// console.log(isDateInRange(date, start, end)) // true
+// console.log(isDateInRange(date, start)) // false (если end нет, то date должна быть равна start)
+
+// ! -------------------------------------------------------
+
+function formatDateAgo(date) {
+  const now = new Date()
+  const diffMs = now - date
+  const diffSec = Math.floor(diffMs / 1000)
+  const diffMin = Math.floor(diffSec / 60)
+  const diffHrs = Math.floor(diffMin / 60)
+  const diffDays = Math.floor(diffHrs / 24)
+
+  const rtf = new Intl.RelativeTimeFormat("ru", {numeric: "auto"})
+
+  if (diffSec < 60) {
+    return "только что"
+  } else if (diffMin < 60) {
+    return rtf.format(-diffMin, "minute")
+  } else if (diffHrs < 24) {
+    return rtf.format(-diffHrs, "hour")
+  } else if (diffDays === 1) {
+    return "вчера"
+  } else if (diffDays < 5) {
+    return rtf.format(-diffDays, "day")
+  } else {
+    const day = date.getDate().toString().padStart(2, "0")
+    const month = (date.getMonth() + 1).toString().padStart(2, "0")
+    const year = date.getFullYear()
+    return `${day}.${month}.${year}`
+  }
+}
+
+const now = new Date()
+
+console.log(formatDateAgo(new Date(now - 30 * 1000)))
+console.log(formatDateAgo(new Date(now - 5 * 60 * 1000)))
+console.log(formatDateAgo(new Date(now - 3 * 86400000)))
+console.log(formatDateAgo(new Date("2023-01-01")))
